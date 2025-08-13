e894b62e903cc7b5bce591d77799f405cf10058f
DUGINISAISHARAN <duginisaisharan@gmail.com>
2025-08-13 21:32:37 +0530

    Revert "Update server.py"
    This reverts commit 0c3f48ac627637d6d177ec21ea86f2a1c57e43c1.

--- server.py
+++ server.py
@@ -21,7 +21,7 @@ os.makedirs(SCREENSHOT_FOLDER, exist_ok=True)
 # Track last screenshot timestamp
 last_screenshot_time = 0
 
-@app.route("/test")
+@app.route("/")
 def index():
     """ Renders the main page """
     return render_template("index.html")
